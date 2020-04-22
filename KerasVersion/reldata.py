1e90c0209bf03edd9a81868cbf78d1c01b5d5f66
Dmitriy Dligach <Dima@Bukowski.local>
2020-04-22 12:47:24 -0500

    option to control how many training examples to make (for debugging)

--- KerasVersion/reldata.py
+++ KerasVersion/reldata.py
@@ -85,7 +85,7 @@ class RelData:
     type_system_file = open(type_system_path, 'rb')
     type_system = load_typesystem(type_system_file)
 
-    for xmi_path in glob.glob(self.xmi_dir + '*.xmi'):
+    for xmi_path in glob.glob(self.xmi_dir + '*.xmi')[:]:
 
       # does this xmi belong to the sought partition?
       xmi_file_name = xmi_path.split('/')[-1]
