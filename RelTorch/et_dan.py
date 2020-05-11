c0abe441dd7fef1c5990d2d954e1a263acd97142
Dima <ddligach@luc.edu>
2020-05-11 15:18:42 -0500

    added droppout and params obtained from config file

--- RelTorch/et_dan.py
+++ RelTorch/et_dan.py
@@ -18,13 +18,15 @@ from sklearn.metrics import f1_score
 
 class DeepAveragingNetwork(nn.Module):
 
-  def __init__(self, vocab_size, embed_dim, num_class):
+  def __init__(self, embed_dim=100, num_class=3):
     """Constructor"""
 
     super().__init__()
+    tok = BertTokenizer.from_pretrained('bert-base-uncased')
 
-    self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=False)
-    self.fc = nn.Linear(embed_dim, num_class)
+    self.embedding_bag = nn.EmbeddingBag(tok.vocab_size, embed_dim)
+    self.dropout = torch.nn.Dropout(0.1)
+    self.linear = nn.Linear(embed_dim, num_class)
 
     self.init_weights()
 
@@ -32,15 +34,20 @@ class DeepAveragingNetwork(nn.Module):
     """Weight initialization"""
 
     initrange = 0.5
-    self.embedding.weight.data.uniform_(-initrange, initrange)
-    self.fc.weight.data.uniform_(-initrange, initrange)
-    self.fc.bias.data.zero_()
+    self.embedding_bag.weight.data.uniform_(-initrange, initrange)
+    self.linear.weight.data.uniform_(-initrange, initrange)
+    self.linear.bias.data.zero_()
 
-  def forward(self, text):
+  def forward(self, texts):
     """Forward pass"""
 
-    embedded = self.embedding(text)
-    logits = self.fc(embedded)
+    # if input is 2D of shape (B, N), it will be treated
+    # as B bags (sequences) each of fixed length N, and
+    # this will return B values aggregated
+
+    embedded = self.embedding_bag(texts)
+    dropped = self.dropout(embedded)
+    logits = self.linear(dropped)
 
     return logits
 
@@ -56,31 +63,27 @@ def performance_metrics(labels, predictions):
   print('f1[contains average] = %.3f' % contains_f1)
 
 def to_inputs(texts, pad_token=0):
-  """Converts texts into input matrices required by BERT"""
+  """Converts texts into input matrices"""
 
   tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
   rows = [tokenizer.encode(text, add_special_tokens=True) for text in texts]
   shape = (len(rows), max(len(row) for row in rows))
   token_ids = np.full(shape=shape, fill_value=pad_token)
-  is_token = np.zeros(shape=shape)
 
   for i, row in enumerate(rows):
     token_ids[i, :len(row)] = row
-    is_token[i, :len(row)] = 1
-
   token_ids = torch.tensor(token_ids)
-  is_token = torch.tensor(is_token)
 
-  return token_ids, is_token
+  return token_ids
 
 def make_data_loader(texts, labels, sampler):
   """DataLoader objects for train or dev/test sets"""
 
-  input_ids, attention_masks = to_inputs(texts)
+  input_ids = to_inputs(texts)
   labels = torch.tensor(labels)
 
-  tensor_dataset = TensorDataset(input_ids, attention_masks, labels)
+  tensor_dataset = TensorDataset(input_ids, labels)
   rnd_or_seq_sampler = sampler(tensor_dataset)
 
   data_loader = DataLoader(
@@ -90,39 +93,22 @@ def make_data_loader(texts, labels, sampler):
 
   return data_loader
 
-def make_optimizer_and_scheduler(model):
-  """This is still a mystery to me"""
-
-  no_decay = ['bias', 'LayerNorm.weight']
-  optimizer_grouped_parameters = [
-      {'params': [p for n, p in model.named_parameters() \
-        if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
-      {'params': [p for n, p in model.named_parameters() \
-        if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
-  optimizer = AdamW(
-    params=optimizer_grouped_parameters,
-    lr=cfg.getfloat('bert', 'lr'))
-  scheduler = get_linear_schedule_with_warmup(
-    optimizer,
-    num_warmup_steps=100,
-    num_training_steps=1000)
-
-  return optimizer, scheduler
-
 def train(model, train_loader, device):
   """Training routine"""
 
   cross_entropy_loss = torch.nn.CrossEntropyLoss()
-  optimizer = torch.optim.Adam(model.parameters())
-  scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
+  optimizer = torch.optim.Adam(
+    model.parameters(),
+    lr=cfg.getfloat('bert', 'lr'))
+  # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
 
-  for epoch in range(5):
+  for epoch in range(cfg.getint('bert', 'num_epochs')):
     model.train()
     train_loss, num_train_steps = 0, 0
 
     for batch in train_loader:
       batch = tuple(t.to(device) for t in batch)
-      batch_inputs, _, batch_labels = batch
+      batch_inputs, batch_labels = batch
       optimizer.zero_grad()
 
       logits = model(batch_inputs)
@@ -130,9 +116,8 @@ def train(model, train_loader, device):
       loss = cross_entropy_loss(logits, batch_labels)
       loss.backward()
 
-      # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
       optimizer.step()
-      scheduler.step()
+      # scheduler.step()
 
       train_loss += loss.item()
       num_train_steps += 1
@@ -149,7 +134,7 @@ def evaluate(model, data_loader, device):
 
   for batch in data_loader:
     batch = tuple(t.to(device) for t in batch)
-    batch_inputs, batch_masks, batch_labels = batch
+    batch_inputs, batch_labels = batch
 
     with torch.no_grad():
       logits = model(batch_inputs)
@@ -168,34 +153,32 @@ def evaluate(model, data_loader, device):
 def main():
   """Fine-tune bert"""
 
-  # BertTokenizer.from_pretrained('bert-base-uncased')
-  # print('vocab size:', BertTokenizer.vocab_size)
+  dan_model = DeepAveragingNetwork()
 
-  dan = DeepAveragingNetwork(30522, 300, 3)
-
-  # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
   if torch.cuda.is_available():
     device = torch.device('cuda')
-    dan.cuda()
+    dan_model.cuda()
   else:
     device = torch.device('cpu')
-    dan.cpu()
+    dan_model.cpu()
 
   train_data = reldata.RelData(
     os.path.join(base, cfg.get('data', 'xmi_dir')),
-    partition='train')
+    partition='train',
+    n_files=cfg.get('data', 'n_files'))
   texts, labels = train_data.event_time_relations()
   train_loader = make_data_loader(texts, labels, RandomSampler)
 
-  train(dan, train_loader, device)
+  train(dan_model, train_loader, device)
 
   dev_data = reldata.RelData(
     os.path.join(base, cfg.get('data', 'xmi_dir')),
-    partition='dev')
+    partition='dev',
+    n_files=cfg.get('data', 'n_files'))
   texts, labels = dev_data.event_time_relations()
-  dev_loader = make_data_loader(texts, labels, sampler=SequentialSampler)
+  dev_loader = make_data_loader(texts, labels, SequentialSampler)
 
-  evaluate(dan, dev_loader, device)
+  evaluate(dan_model, dev_loader, device)
 
 if __name__ == "__main__":
 
