2c57f8852dfbf262012abf62420f52f52757bd27
Dima <ddligach@luc.edu>
2020-05-11 12:55:36 -0500

    added an averaging network classifier for event time

---
+++ RelTorch/et_dan.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+
+import sys
+sys.dont_write_bytecode = True
+
+import torch
+import torch.nn as nn
+
+from transformers import AdamW, get_linear_schedule_with_warmup
+from transformers import BertTokenizer
+
+from torch.utils.data import TensorDataset, DataLoader
+from torch.utils.data import RandomSampler, SequentialSampler
+
+import numpy as np
+import os, configparser, reldata
+from sklearn.metrics import f1_score
+
+class DeepAveragingNetwork(nn.Module):
+
+  def __init__(self, vocab_size, embed_dim, num_class):
+    """Constructor"""
+
+    super().__init__()
+
+    self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=False)
+    self.fc = nn.Linear(embed_dim, num_class)
+
+    self.init_weights()
+
+  def init_weights(self):
+    """Weight initialization"""
+
+    initrange = 0.5
+    self.embedding.weight.data.uniform_(-initrange, initrange)
+    self.fc.weight.data.uniform_(-initrange, initrange)
+    self.fc.bias.data.zero_()
+
+  def forward(self, text):
+    """Forward pass"""
+
+    embedded = self.embedding(text)
+    logits = self.fc(embedded)
+
+    return logits
+
+def performance_metrics(labels, predictions):
+  """Report performance metrics"""
+
+  f1 = f1_score(labels, predictions, average=None)
+  for index, f1 in enumerate(f1):
+    print('f1[%s] = %.3f' % (reldata.int2label[index], f1))
+
+  ids = [reldata.label2int['CONTAINS'], reldata.label2int['CONTAINS-1']]
+  contains_f1 = f1_score(labels, predictions, labels=ids, average='micro')
+  print('f1[contains average] = %.3f' % contains_f1)
+
+def to_inputs(texts, pad_token=0):
+  """Converts texts into input matrices required by BERT"""
+
+  tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+  rows = [tokenizer.encode(text, add_special_tokens=True) for text in texts]
+  shape = (len(rows), max(len(row) for row in rows))
+  token_ids = np.full(shape=shape, fill_value=pad_token)
+  is_token = np.zeros(shape=shape)
+
+  for i, row in enumerate(rows):
+    token_ids[i, :len(row)] = row
+    is_token[i, :len(row)] = 1
+
+  token_ids = torch.tensor(token_ids)
+  is_token = torch.tensor(is_token)
+
+  return token_ids, is_token
+
+def make_data_loader(texts, labels, sampler):
+  """DataLoader objects for train or dev/test sets"""
+
+  input_ids, attention_masks = to_inputs(texts)
+  labels = torch.tensor(labels)
+
+  tensor_dataset = TensorDataset(input_ids, attention_masks, labels)
+  rnd_or_seq_sampler = sampler(tensor_dataset)
+
+  data_loader = DataLoader(
+    tensor_dataset,
+    sampler=rnd_or_seq_sampler,
+    batch_size=cfg.getint('bert', 'batch_size'))
+
+  return data_loader
+
+def make_optimizer_and_scheduler(model):
+  """This is still a mystery to me"""
+
+  no_decay = ['bias', 'LayerNorm.weight']
+  optimizer_grouped_parameters = [
+      {'params': [p for n, p in model.named_parameters() \
+        if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+      {'params': [p for n, p in model.named_parameters() \
+        if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}]
+  optimizer = AdamW(
+    params=optimizer_grouped_parameters,
+    lr=cfg.getfloat('bert', 'lr'))
+  scheduler = get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=100,
+    num_training_steps=1000)
+
+  return optimizer, scheduler
+
+def train(model, train_loader, device):
+  """Training routine"""
+
+  cross_entropy_loss = torch.nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(model.parameters())
+  scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.9)
+
+  for epoch in range(5):
+    model.train()
+    train_loss, num_train_steps = 0, 0
+
+    for batch in train_loader:
+      batch = tuple(t.to(device) for t in batch)
+      batch_inputs, _, batch_labels = batch
+      optimizer.zero_grad()
+
+      logits = model(batch_inputs)
+
+      loss = cross_entropy_loss(logits, batch_labels)
+      loss.backward()
+
+      # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+      optimizer.step()
+      scheduler.step()
+
+      train_loss += loss.item()
+      num_train_steps += 1
+
+    print('epoch: %d, loss: %.4f' % (epoch, train_loss / num_train_steps))
+
+def evaluate(model, data_loader, device):
+  """Evaluation routine"""
+
+  model.eval()
+
+  all_labels = []
+  all_predictions = []
+
+  for batch in data_loader:
+    batch = tuple(t.to(device) for t in batch)
+    batch_inputs, batch_masks, batch_labels = batch
+
+    with torch.no_grad():
+      logits = model(batch_inputs)
+
+    batch_logits = logits.detach().cpu().numpy()
+    batch_labels = batch_labels.to('cpu').numpy()
+    batch_preds = np.argmax(batch_logits, axis=1)
+
+    all_labels.extend(batch_labels.tolist())
+    all_predictions.extend(batch_preds.tolist())
+
+  performance_metrics(all_labels, all_predictions)
+
+  return all_predictions
+
+def main():
+  """Fine-tune bert"""
+
+  # BertTokenizer.from_pretrained('bert-base-uncased')
+  # print('vocab size:', BertTokenizer.vocab_size)
+
+  dan = DeepAveragingNetwork(30522, 300, 3)
+
+  # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  if torch.cuda.is_available():
+    device = torch.device('cuda')
+    dan.cuda()
+  else:
+    device = torch.device('cpu')
+    dan.cpu()
+
+  train_data = reldata.RelData(
+    os.path.join(base, cfg.get('data', 'xmi_dir')),
+    partition='train')
+  texts, labels = train_data.event_time_relations()
+  train_loader = make_data_loader(texts, labels, RandomSampler)
+
+  train(dan, train_loader, device)
+
+  dev_data = reldata.RelData(
+    os.path.join(base, cfg.get('data', 'xmi_dir')),
+    partition='dev')
+  texts, labels = dev_data.event_time_relations()
+  dev_loader = make_data_loader(texts, labels, sampler=SequentialSampler)
+
+  evaluate(dan, dev_loader, device)
+
+if __name__ == "__main__":
+
+  cfg = configparser.ConfigParser()
+  cfg.read(sys.argv[1])
+  base = os.environ['DATA_ROOT']
+
+  main()
