cf3309ceabede0cc31950238077a1c65c210f081
Dima <ddligach@luc.edu>
2020-05-15 11:54:37 -0500

    moved most of the transformer configuration to a config file

--- Rel/et_trans.py
+++ Rel/et_trans.py
@@ -17,23 +17,71 @@ import os, configparser, math
 
 import reldata, metrics
 
+class Transformer(nn.Module):
+  """A transformative experience"""
+
+  def __init__(self, num_classes=3):
+    """We have some of the best constructors in the world"""
+
+    super(Transformer, self).__init__()
+
+    tok = BertTokenizer.from_pretrained('bert-base-uncased')
+    vocab_size = tok.vocab_size # the reason we need bert tokenizer
+
+    self.embedding = nn.Embedding(
+      num_embeddings=vocab_size,
+      embedding_dim=cfg.getint('model', 'emb_dim'))
+
+    self.position = PositionalEncoding(
+      d_model=cfg.getint('model', 'emb_dim'))
+
+    encoder_layer = nn.TransformerEncoderLayer(
+      d_model=cfg.getint('model', 'emb_dim'),
+      nhead=cfg.getint('model', 'num_heads'),
+      dim_feedforward=cfg.getint('model', 'feedforw_dim'))
+
+    self.trans_encoder = nn.TransformerEncoder(
+      encoder_layer=encoder_layer,
+      num_layers=cfg.getint('model', 'num_layers'))
+
+    self.dropout = nn.Dropout(cfg.getfloat('model', 'dropout'))
+
+    self.linear = nn.Linear(
+      in_features=cfg.getint('model', 'emb_dim'),
+      out_features=num_classes)
+
+  def forward(self, texts):
+    """Moving forward"""
+
+    sqrtn = math.sqrt(cfg.getint('model', 'emb_dim'))
+    output = self.embedding(texts) * sqrtn
+    output = self.position(output)
+
+    # encoder input: (seq_len, batch_size, emb_dim)
+    # encoder output: (seq_len, batch_size, emb_dim)
+    output = output.permute(1, 0, 2)
+    output = self.trans_encoder(output)
+    output = output[0, :, :]
+
+    output = self.dropout(output)
+    output = self.linear(output)
+
+    return output
+
 class PositionalEncoding(nn.Module):
   """That's my position"""
 
-  def __init__(self, d_model, dropout=0.1, max_len=5000):
+  def __init__(self, d_model, max_len=5000):
     """Deconstructing the construct"""
 
     super(PositionalEncoding, self).__init__()
-    self.dropout = nn.Dropout(p=dropout)
+    self.dropout = nn.Dropout(p=0.1)
 
     pe = torch.zeros(max_len, d_model)
-
     position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
     div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-
     pe[:, 0::2] = torch.sin(position * div_term)
     pe[:, 1::2] = torch.cos(position * div_term)
-
     pe = pe.unsqueeze(0).transpose(0, 1)
     self.register_buffer('pe', pe)
 
@@ -43,56 +91,6 @@ class PositionalEncoding(nn.Module):
     x = x + self.pe[:x.size(0), :]
 
     return self.dropout(x)
-
-class Transformer(nn.Module):
-  """A transformative experience"""
-
-  def __init__(self, embed_dim=128, num_class=3):
-    """We have some of the best constructors in the world"""
-
-    super(Transformer, self).__init__()
-    tok = BertTokenizer.from_pretrained('bert-base-uncased')
-
-    self.position = PositionalEncoding(
-      d_model=embed_dim,
-      dropout=0.5)
-
-    self.embedding = nn.Embedding(
-      num_embeddings=tok.vocab_size,
-      embedding_dim=embed_dim)
-
-    encoder_layer = nn.TransformerEncoderLayer(
-      d_model=embed_dim,
-      nhead=1,
-      dim_feedforward=512)
-    self.transformer_encoder = nn.TransformerEncoder(
-      encoder_layer=encoder_layer,
-      num_layers=1)
-
-    self.dropout = nn.Dropout(0.25)
-
-    self.linear = nn.Linear(
-      in_features=embed_dim,
-      out_features=num_class)
-
-  def forward(self, texts):
-    """Moving forward"""
-
-    output = self.embedding(texts) * math.sqrt(128) # fix this
-
-    output = self.position(output)
-
-    output = output.permute(1, 0, 2)
-
-    # transformer encoder output: (154, 32, 128)
-    output = self.transformer_encoder(output)
-    output = output[0, :, :]
-
-    output = self.dropout(output)
-
-    output = self.linear(output)
-
-    return output
 
 def to_inputs(texts, pad_token=0):
   """Converts texts into input matrices"""
@@ -121,7 +119,7 @@ def make_data_loader(texts, labels, sampler):
   data_loader = DataLoader(
     tensor_dataset,
     sampler=rnd_or_seq_sampler,
-    batch_size=cfg.getint('bert', 'batch_size'))
+    batch_size=cfg.getint('model', 'batch_size'))
 
   return data_loader
 
@@ -131,9 +129,9 @@ def train(model, train_loader, device):
   cross_entropy_loss = torch.nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(
     model.parameters(),
-    lr=cfg.getfloat('bert', 'lr'))
+    lr=cfg.getfloat('model', 'lr'))
 
-  for epoch in range(cfg.getint('bert', 'num_epochs')):
+  for epoch in range(cfg.getint('model', 'num_epochs')):
     model.train()
     train_loss, num_train_steps = 0, 0
 
