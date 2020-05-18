7d5d10c21f5bd821cebf4fd0682e5f152f1a9d1d
Dima <ddligach@luc.edu>
2020-05-18 13:06:25 -0500

    renamed model to transformer classifier

--- EventTime/trans.py
+++ EventTime/trans.py
@@ -24,13 +24,13 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(2020)
 random.seed(2020)
 
-class Transformer(nn.Module):
+class TransformerClassifier(nn.Module):
   """A transformative experience"""
 
   def __init__(self, num_classes=3):
     """We have some of the best constructors in the world"""
 
-    super(Transformer, self).__init__()
+    super(TransformerClassifier, self).__init__()
 
     tok = BertTokenizer.from_pretrained('bert-base-uncased')
     vocab_size = tok.vocab_size # the reason we need bert tokenizer
@@ -82,15 +82,19 @@ class PositionalEncoding(nn.Module):
     """Deconstructing the construct"""
 
     super(PositionalEncoding, self).__init__()
+
     self.dropout = nn.Dropout(p=0.1)
 
     pe = torch.zeros(max_len, embedding_dim)
+
     position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
     div_term = torch.exp(torch.arange(0, embedding_dim, 2).float() * \
                          (-math.log(10000.0) / embedding_dim))
+
     pe[:, 0::2] = torch.sin(position * div_term)
     pe[:, 1::2] = torch.cos(position * div_term)
     pe = pe.unsqueeze(0).transpose(0, 1)
+
     self.register_buffer('pe', pe)
 
   def forward(self, x):
@@ -187,7 +191,7 @@ def evaluate(model, data_loader, device):
 def main():
   """Fine-tune bert"""
 
-  model = Transformer()
+  model = TransformerClassifier()
 
   if torch.cuda.is_available():
     device = torch.device('cuda')
