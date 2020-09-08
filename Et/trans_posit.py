088fddf898b31219827495d522f197245b6e2dcc
Dima <ddligach@luc.edu>
2020-09-08 14:01:48 -0500

    added a version of transfomer that uses bert position embeddings

---
+++ Et/trans_posit.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+
+import sys
+sys.dont_write_bytecode = True
+sys.path.append('../Lib/')
+
+import torch
+import torch.nn as nn
+
+from tokenizers import CharBPETokenizer
+from transformers import get_linear_schedule_with_warmup
+from transformers import BertModel, BertPreTrainedModel
+
+import numpy as np
+import os, configparser, math, random
+
+import reldata, metrics, utils
+
+# deterministic determinism
+torch.manual_seed(2020)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(2020)
+random.seed(2020)
+
+class TransformerClassifier(nn.Module):
+  """A transformative experience"""
+
+  def __init__(self, num_classes=3):
+    """We have some of the best constructors in the world"""
+
+    super(TransformerClassifier, self).__init__()
+
+    tokenizer = CharBPETokenizer(
+      '../Tokenize/thyme-tokenizer-vocab.json',
+      '../Tokenize/thyme-tokenizer-merges.txt')
+    vocab_size = tokenizer.get_vocab_size()
+
+    self.embedding = nn.Embedding(
+      num_embeddings=vocab_size,
+      embedding_dim=cfg.getint('model', 'emb_dim'))
+
+    self.position = PositionalEncoding.from_pretrained(
+      'bert-base-uncased')
+
+    encoder_layer = nn.TransformerEncoderLayer(
+      d_model=cfg.getint('model', 'emb_dim'),
+      nhead=cfg.getint('model', 'num_heads'),
+      dim_feedforward=cfg.getint('model', 'feedforw_dim'))
+
+    self.trans_encoder = nn.TransformerEncoder(
+      encoder_layer=encoder_layer,
+      num_layers=cfg.getint('model', 'num_layers'))
+
+    self.dropout = nn.Dropout(cfg.getfloat('model', 'dropout'))
+
+    self.linear = nn.Linear(
+      in_features=cfg.getint('model', 'emb_dim'),
+      out_features=num_classes)
+
+  def forward(self, texts, attention_mask):
+    """Moving forward"""
+
+    sqrtn = math.sqrt(cfg.getint('model', 'emb_dim'))
+    output = self.embedding(texts) * sqrtn
+    output = self.position(texts, output)
+
+    # encoder input: (seq_len, batch_size, emb_dim)
+    # encoder output: (seq_len, batch_size, emb_dim)
+    output = output.permute(1, 0, 2)
+    output = self.trans_encoder(output, attention_mask)
+
+    # extract CLS token only
+    # output = output[0, :, :]
+
+    # average pooling
+    output = torch.mean(output, dim=0)
+
+    output = self.dropout(output)
+    output = self.linear(output)
+
+    return output
+
+class PositionalEncoding(BertPreTrainedModel):
+  """Linear layer on top of pre-trained BERT"""
+
+  def __init__(self, config):
+    """Constructor"""
+
+    super(PositionalEncoding, self).__init__(config)
+    self.bert = BertModel(config)
+
+  def forward(self, token_ids, token_embeddings):
+    """Forward pass"""
+
+    seq_length = token_ids.size(1)
+    position_ids = torch.arange(seq_length, dtype=torch.long, device=token_ids.device)
+    position_ids = position_ids.unsqueeze(0).expand_as(token_ids)
+    position_embeddings = self.bert.embeddings.position_embeddings(position_ids)
+    token_embeddings = token_embeddings + position_embeddings
+
+    # return self.dropout(x)
+    return token_embeddings
+
+def train(model, train_loader, val_loader, weights):
+  """Training routine"""
+
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  model.to(device)
+
+  weights = weights.to(device)
+  cross_entropy_loss = torch.nn.CrossEntropyLoss(weights)
+
+  optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=cfg.getfloat('model', 'lr'))
+
+  scheduler = get_linear_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps=100,
+    num_training_steps=1000)
+
+  for epoch in range(1, cfg.getint('model', 'num_epochs') + 1):
+    model.train()
+    train_loss, num_train_steps = 0, 0
+
+    for batch in train_loader:
+      batch = tuple(t.to(device) for t in batch)
+      batch_inputs, batch_mask, batch_labels = batch
+      batch_mask = batch_mask.repeat(cfg.getint('model', 'num_heads'), 1, 1)
+      optimizer.zero_grad()
+
+      logits = model(batch_inputs, batch_mask)
+      loss = cross_entropy_loss(logits, batch_labels)
+      loss.backward()
+
+      torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+      optimizer.step()
+      scheduler.step()
+
+      train_loss += loss.item()
+      num_train_steps += 1
+
+    av_loss = train_loss / num_train_steps
+    val_loss, f1 = evaluate(model, val_loader, weights)
+    print('ep: %d, steps: %d, tr loss: %.3f, val loss: %.3f, val f1: %.3f' % \
+          (epoch, num_train_steps, av_loss, val_loss, f1))
+
+def evaluate(model, data_loader, weights, suppress_output=True):
+  """Evaluation routine"""
+
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  weights = weights.to(device)
+  model.to(device)
+
+  cross_entropy_loss = torch.nn.CrossEntropyLoss(weights)
+  total_loss, num_steps = 0, 0
+
+  model.eval()
+
+  all_labels = []
+  all_predictions = []
+
+  for batch in data_loader:
+    batch = tuple(t.to(device) for t in batch)
+    batch_inputs, batch_mask, batch_labels = batch
+    batch_mask = batch_mask.repeat(cfg.getint('model', 'num_heads'), 1, 1)
+
+    with torch.no_grad():
+      logits = model(batch_inputs, batch_mask)
+      loss = cross_entropy_loss(logits, batch_labels)
+
+    batch_logits = logits.detach().cpu().numpy()
+    batch_labels = batch_labels.to('cpu').numpy()
+    batch_preds = np.argmax(batch_logits, axis=1)
+
+    all_labels.extend(batch_labels.tolist())
+    all_predictions.extend(batch_preds.tolist())
+
+    total_loss += loss.item()
+    num_steps += 1
+
+  f1 = metrics.f1(all_labels,
+                  all_predictions,
+                  reldata.int2label,
+                  reldata.label2int,
+                  suppress_output)
+
+  return total_loss / num_steps, f1
+
+def main():
+  """Fine-tune bert"""
+
+  train_data = reldata.RelData(
+    os.path.join(base, cfg.get('data', 'xmi_dir')),
+    partition='train',
+    n_files=cfg.get('data', 'n_files'))
+  tr_texts, tr_labels = train_data.event_time_relations()
+  train_loader = utils.make_data_loader(
+    tr_texts,
+    tr_labels,
+    cfg.getint('model', 'batch_size'),
+    cfg.getint('data', 'max_len'),
+    'train',
+    utils.to_transformer_inputs)
+
+  val_data = reldata.RelData(
+    os.path.join(base, cfg.get('data', 'xmi_dir')),
+    partition='dev',
+    n_files=cfg.get('data', 'n_files'))
+  val_texts, val_labels = val_data.event_time_relations()
+  val_loader = utils.make_data_loader(
+    val_texts,
+    val_labels,
+    cfg.getint('model', 'batch_size'),
+    cfg.getint('data', 'max_len'),
+    'dev',
+    utils.to_transformer_inputs)
+
+  print('loaded %d training and %d validation samples' % \
+        (len(tr_texts), len(val_texts)))
+
+  model = TransformerClassifier()
+
+  label_counts = torch.bincount(torch.IntTensor(tr_labels))
+  weights = len(tr_labels) / (2.0 * label_counts)
+
+  train(model, train_loader, val_loader, weights)
+  evaluate(model, val_loader, weights, suppress_output=False)
+
+if __name__ == "__main__":
+
+  cfg = configparser.ConfigParser()
+  cfg.read(sys.argv[1])
+  base = os.environ['DATA_ROOT']
+
+  main()
