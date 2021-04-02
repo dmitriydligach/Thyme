2a132ae3f166b213a519336224df77dc96d2be5f
Dima <ddligach@luc.edu>
2021-04-02 10:35:06 -0500

    added mapping sections to relations

--- Misc/dataset_rel.py
+++ Misc/dataset_rel.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
+import sys, re, glob
+
 sys.dont_write_bytecode = True
 sys.path.append('../Anafora')
 
@@ -21,30 +22,58 @@ class RelData:
     self.text_dir = text_dir
     self.xml_regex = xml_regex
 
-  def read(self):
+    self.map_notes_to_relations()
+    self.map_sections_to_relations()
+
+  def map_sections_to_relations(self):
+    """Sectionize and index"""
+
+    for note_path in glob.glob(self.text_dir + 'ID*'):
+
+      # some notes weren't annotated
+      if note_path not in self.note2args:
+        continue
+
+      print(note_path)
+      note_text = open(note_path).read()
+      regex_str = r'\[start section id=\"(.+)"\](.*?)\[end section id=\"\1"\]'
+
+      for match in re.finditer(regex_str, note_text, re.DOTALL):
+
+        # section offsets
+        sec_start, sec_end = match.start(2), match.end(2)
+        print(note_text[sec_start:sec_end])
+
+        # look for relation arguments contained in this section
+        for src_spans, targ_spans in self.note2args[note_path]:
+          src_start, src_end = src_spans
+          targ_start, targ_end = targ_spans
+          if src_start >= sec_start and src_end <= sec_end:
+            src = note_text[src_start:src_end]
+            targ = note_text[targ_start:targ_end]
+            print('contains(%s, %s)' % (src, targ))
+
+  def map_notes_to_relations(self):
     """Make x, y etc."""
 
-    for sub_dir, text_name, file_names in anafora.walk(self.xml_dir, self.xml_regex):
+    # key: note path, value: (source, target) tuples
+    self.note2args = {}
 
+    for sub_dir, text_name, file_names in anafora.walk(self.xml_dir, self.xml_regex):
+      text_path = os.path.join(self.text_dir, text_name)
       xml_path = os.path.join(self.xml_dir, sub_dir, file_names[0])
       ref_data = anafora.AnaforaData.from_file(xml_path)
 
-      text_path = os.path.join(self.text_dir, text_name)
-      text = open(text_path).read()
-
+      rel_args = []
       for rel in ref_data.annotations.select_type('TLINK'):
         source = rel.properties['Source']
         target = rel.properties['Target']
         label = rel.properties['Type']
 
-        start, end = source.spans[0]
-        source_text = text[start:end]
+        if label == 'CONTAINS':
+          rel_args.append((source.spans[0], target.spans[0]))
 
-        start, end = target.spans[0]
-        target_text = text[start:end]
-
-        print('%s(%s, %s)' % (label, source_text, target_text))
-
+      self.note2args[text_path] = rel_args
 
 if __name__ == "__main__":
 
@@ -54,5 +83,3 @@ if __name__ == "__main__":
     xml_dir=os.path.join(base, 'Thyme/Official/thymedata/coloncancer/Train/'),
     text_dir=os.path.join(base, 'Thyme/Text/train/'),
     xml_regex='.*[.]Temporal.*[.]xml')
-
-  rel_data.read()
