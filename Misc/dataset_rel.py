2c19f4bf87d185e99a94992c7e91cb951c7b1f2d
Dima <ddligach@luc.edu>
2021-04-05 11:10:08 -0500

    added generating section/rel list pairs

--- Misc/dataset_rel.py
+++ Misc/dataset_rel.py
@@ -22,49 +22,57 @@ class RelData:
     self.text_dir = text_dir
     self.xml_regex = xml_regex
 
+    self.inputs = []
+    self.outputs = []
+
     self.map_notes_to_relations()
     self.map_sections_to_relations()
 
   def map_sections_to_relations(self):
     """Sectionize and index"""
 
+    # iterate over notes and sectionize them
     for note_path in glob.glob(self.text_dir + 'ID*'):
 
       # some notes weren't annotated
       if note_path not in self.note2args:
         continue
 
-      print(note_path)
       note_text = open(note_path).read()
       regex_str = r'\[start section id=\"(.+)"\](.*?)\[end section id=\"\1"\]'
 
+      # iterate over sections
       for match in re.finditer(regex_str, note_text, re.DOTALL):
-
-        # section offsets
         sec_start, sec_end = match.start(2), match.end(2)
-        print(note_text[sec_start:sec_end])
 
-        # look for relation arguments contained in this section
+        rels_in_section = []
         for src_spans, targ_spans in self.note2args[note_path]:
           src_start, src_end = src_spans
           targ_start, targ_end = targ_spans
-          if src_start >= sec_start and src_end <= sec_end:
+
+          if src_start >= sec_start and src_end <= sec_end and \
+             targ_start >= sec_start and targ_end <= sec_end:
             src = note_text[src_start:src_end]
             targ = note_text[targ_start:targ_end]
-            print('contains(%s, %s)' % (src, targ))
+            rels_in_section.append('CONTAINS(%s, %s)' % (src, targ))
+
+        self.inputs.append(match.group(2))
+        self.outputs.append(' '.join(rels_in_section))
 
   def map_notes_to_relations(self):
-    """Make x, y etc."""
+    """Map note paths to relation argument offsets"""
 
     # key: note path, value: (source, target) tuples
     self.note2args = {}
 
     for sub_dir, text_name, file_names in anafora.walk(self.xml_dir, self.xml_regex):
-      text_path = os.path.join(self.text_dir, text_name)
+      note_path = os.path.join(self.text_dir, text_name)
       xml_path = os.path.join(self.xml_dir, sub_dir, file_names[0])
       ref_data = anafora.AnaforaData.from_file(xml_path)
 
+      # (src_start, src_end, targ_start, targ_end) tuples
       rel_args = []
+
       for rel in ref_data.annotations.select_type('TLINK'):
         source = rel.properties['Source']
         target = rel.properties['Target']
@@ -73,7 +81,7 @@ class RelData:
         if label == 'CONTAINS':
           rel_args.append((source.spans[0], target.spans[0]))
 
-      self.note2args[text_path] = rel_args
+      self.note2args[note_path] = rel_args
 
 if __name__ == "__main__":
 
@@ -83,3 +91,6 @@ if __name__ == "__main__":
     xml_dir=os.path.join(base, 'Thyme/Official/thymedata/coloncancer/Train/'),
     text_dir=os.path.join(base, 'Thyme/Text/train/'),
     xml_regex='.*[.]Temporal.*[.]xml')
+
+  print(rel_data.inputs[4])
+  print(rel_data.outputs[4])
