feeba07ce49c7acb89c4e334b619957c4963dd3c
Dima <ddligach@luc.edu>
2021-05-23 10:15:16 -0500

    added closure option to eval script

--- T5Rel/eval.sh
+++ T5Rel/eval.sh
@@ -6,6 +6,10 @@ CURRENT_DIR=$PWD
 
 cd ../Anafora/
 
+echo 'no closure'
 python -m anafora.evaluate -r $DATA_ROOT$REF -p $PRED -x ".*[.]Temporal.*[.]xml" -i TLINK:Type:CONTAINS
+
+echo 'with closure'
+python -m anafora.evaluate -r $DATA_ROOT$REF -p $PRED -x ".*[.]Temporal.*[.]xml" -i TLINK:Type:CONTAINS --temporal-closure
 
 cd $CURRENT_DIR
