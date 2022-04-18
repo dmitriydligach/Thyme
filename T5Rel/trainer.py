5416cdb7fd7c09e1924a949b9791ad1962e6a37a
Dima <ddligach@luc.edu>
2022-04-18 10:07:34 -0500

    synced trainer with latest finetune.py

--- T5Rel/trainer.py
+++ T5Rel/trainer.py
@@ -12,20 +12,21 @@ import random, argparse, os, shutil, importlib
 
 # deterministic determinism
 torch.manual_seed(2020)
-# torch.backends.cudnn.deterministic = True
-# torch.backends.cudnn.benchmark = False
 random.seed(2020)
+
+# new tokens to be added to tokenizer
+new_tokens = ['<t>', '</t>', '<e>', '</e>']
 
 def main():
   """Fine-tune on summarization data"""
+
+  # import data provider (e.g. dtr, rel, or events)
+  data = importlib.import_module(args.data_reader)
 
   # need this to save a fine-tuned model
   if os.path.isdir(args.model_dir):
     shutil.rmtree(args.model_dir)
   os.mkdir(args.model_dir)
-
-  # import data provider (e.g. dtr, rel, or events)
-  data = importlib.import_module(args.data_reader)
 
   # load pretrained T5 tokenizer
   tokenizer = T5Tokenizer.from_pretrained(args.model_name)
@@ -33,12 +34,17 @@ def main():
   # load a pretrained T5 model
   model = T5ForConditionalGeneration.from_pretrained(args.model_name)
 
+  # add event markers to tokenizer
+  tokenizer.add_tokens(new_tokens)
+  model.resize_token_embeddings(len(tokenizer))
+
   train_dataset = data.Data(
     xml_dir=args.xml_train_dir,
     text_dir=args.text_train_dir,
     out_dir=args.xml_out_dir,
     xml_regex=args.xml_regex,
     tokenizer=tokenizer,
+    chunk_size=args.chunk_size,
     max_input_length=args.max_input_length,
     max_output_length=args.max_output_length)
 
@@ -48,14 +54,15 @@ def main():
     out_dir=args.xml_out_dir,
     xml_regex=args.xml_regex,
     tokenizer=tokenizer,
+    chunk_size=args.chunk_size,
     max_input_length=args.max_input_length,
     max_output_length=args.max_output_length)
 
   training_args = Seq2SeqTrainingArguments(
     output_dir='./Results',
     num_train_epochs=args.n_epochs,
-    per_device_train_batch_size=args.batch_size,
-    per_device_eval_batch_size=args.batch_size,
+    per_device_train_batch_size=args.train_batch_size,
+    per_device_eval_batch_size=args.gener_batch_size,
     learning_rate=args.learning_rate,
     warmup_steps=100,
     weight_decay=0.01,
@@ -96,16 +103,26 @@ if __name__ == "__main__":
     text_train_dir=os.path.join(base, 'Thyme/Text/train/'),
     xml_test_dir=os.path.join(base, 'Thyme/Official/thymedata/coloncancer/Dev/'),
     text_test_dir=os.path.join(base, 'Thyme/Text/dev/'),
-    xml_regex='.*[.]Temporal.*[.]xml',
     xml_out_dir='./Xml/',
+    xml_regex='.*[.]Temporal.*[.]xml',
     data_reader='dataset_rel',
     model_dir='Model/',
     model_name='t5-base',
+    chunk_size=200,
     max_input_length=512,
     max_output_length=512,
-    learning_rate=1e-3,
-    batch_size=8,
-    n_epochs=5)
+    n_files='all',
+    learning_rate=1e-4,
+    train_batch_size=16,
+    gener_batch_size=128,
+    num_beams=3,
+    weight_decay=0.0001,
+    print_predictions=False,
+    print_metadata=False,
+    print_errors=False,
+    do_train=True,
+    early_stopping=True,
+    n_epochs=2)
   args = argparse.Namespace(**arg_dict)
   print('hyper-parameters: %s\n' % args)
 
